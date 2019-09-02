9c26bbb2d19ce466192f8f33519b7e69cecc61a7
Linda Zhao <lindajzhao@gmail.com>
2019-09-02 13:44:49 -0400

    error and logging

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-// middleware
+const herbsRoute = require("./api/routes/herbs");
+const spellsRoute = require("./api/routes/spells");
+
+// MIDDLEWARE
+
+// logging
+app.use(morgan("dev"));
+
+// routes
+app.use("/herbs", herbsRoute);
+app.use("/spells", spellsRoute);
+
+// error handling
 app.use((req, res, next) => {
-  res.status(200).json({
-    message: "It worked!"
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
   });
 });
 
